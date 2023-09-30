ac9a3c02109b930fe3e6f2ccf6ba766bcca6a523
Ahemd Fayed <ll.ahmedfayed@gmail.com>
2023-09-30 22:54:07 +0300

    init project

--- src/shared/middlewares/hassing.password.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-import {Request, NextFunction} from 'express';
-
-const prisma = new PrismaClient();
-
-prisma.$use(async (params, next: NextFunction) => {
-  if (params.args)
-})
